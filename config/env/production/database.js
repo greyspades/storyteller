0952a680061b8f957b5c415c33ac3ace60e91863
chidera <greyspades99@gmail.com>
2022-05-24 17:24:04 +0100

    attempt 3 at deployment to heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,10 +10,37 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+    //   ssl: {
+    //     rejectUnauthorized: false
+    //   },
+    ssl: {
+        rejectUnauthorized: false, // For self-signed certificates
+    },
+    options: {
+        ssl: false,
+    },
     },
     debug: false,
   },
 });
+
+// module.exports = ({ env }) => ({
+//     defaultConnection: 'default',
+//     connections: {
+//     default: {
+//     connector: 'bookshelf',
+//     settings: {
+//     client:'postgres',
+//     host:`${process.env.DATABASE_HOST}`,
+//     port: `${process.env.DATABASE_PORT}`,
+//     database: `${process.env.DATABASE_NAME}`,
+//     username: `${process.env.DATABASE_USERNAME}`,
+//     password: `${process.env.DATABASE_PASSWORD}`, 
+//     ssl: { "rejectUnauthorized": false }
+//     },
+//     options: {
+//     },
+//     },
+//     },
+//     });  
+    
